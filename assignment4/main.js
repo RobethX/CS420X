54e5476071397b7262d5e334ef9c414274ae4ea2
Rob Chiocchio <RobethX@users.noreply.github.com>
2022-07-11 23:16:07 -0400

    Copy base template code to assignment 4

--- assignment4/main.js
+++ assignment4/main.js
@@ -1,15 +1,79 @@
 // "global" variables
-let gl, uTime, uRes, program, videoTexture, cursorPos
+let gl, uTime, uRes, transformFeedback, 
+    buffer1, buffer2, simulationPosition, copyPosition,
+    textureBack, textureFront, framebuffer,
+    copyProgram, simulationProgram, quad, pane,
+    dimensions = { width:null, height:null },
+    antCount = 1
 
 window.onload = function() {
-    const canvas = document.getElementById( "gl" )
-    gl = canvas.getContext( "webgl" )
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+    const canvas = document.getElementById( 'gl' )
+    gl = canvas.getContext( 'webgl2' )
+    const dim = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight
+    canvas.width  = dimensions.width  = dim
+    canvas.height = dimensions.height = dim 
 
     // define drawing area of canvas. bottom corner, width / height
-    gl.viewport( 0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight )
+    gl.viewport( 0,0,gl.drawingBufferWidth, gl.drawingBufferHeight )
 
+    makeCopyPhase()
+    makeSimulationPhase()
+    makeTextures()
+    makeTweakPane()
+    render()
+}
+
+function makeCopyPhase() {
+    makeCopyShaders()
+    quad = makeCopyBuffer()
+    makeCopyUniforms()
+}
+
+function makeTweakPane() {
+    const PARAMS = {
+        magnify: true,
+        zoom: 0.5,
+        radius: 0.1,
+        wobble: true,
+        speed: 1.0,
+        mix: 0.5,
+        edges: true,
+        threshold: 0.75,
+        color: "#8070FF",
+    };
+
+    pane = new Tweakpane.Pane();
+
+    pane.on("change", (ev)=>{
+        console.log("Pane variable " + JSON.stringify(ev.presetKey) + " changed to " + JSON.stringify(ev.value))
+    })
+}
+
+function makeCopyShaders() {
+    let shaderScript = document.getElementById('copyVertex')
+    let shaderSource = shaderScript.text
+    let vertexShader = gl.createShader( gl.VERTEX_SHADER )
+    gl.shaderSource( vertexShader, shaderSource )
+    gl.compileShader( vertexShader )
+
+    // create fragment shader
+    shaderScript = document.getElementById('copyFragment')
+    shaderSource = shaderScript.text
+    const drawFragmentShader = gl.createShader( gl.FRAGMENT_SHADER )
+    gl.shaderSource( drawFragmentShader, shaderSource )
+    gl.compileShader( drawFragmentShader )
+    console.log( gl.getShaderInfoLog(drawFragmentShader) )
+
+    // create shader program  
+    copyProgram = gl.createProgram()
+    gl.attachShader( copyProgram, vertexShader )
+    gl.attachShader( copyProgram, drawFragmentShader )
+    
+    gl.linkProgram( copyProgram )
+    gl.useProgram( copyProgram )
+}
+
+function makeCopyBuffer() {
     // create a buffer object to store vertices
     const buffer = gl.createBuffer()
 
@@ -18,119 +82,184 @@ window.onload = function() {
 
     const triangles = new Float32Array([
         -1, -1,
-        1,  -1,
-        -1, 1,
-        -1, 1,
-        1, -1,
-        1, 1
+         1, -1,
+        -1,  1,
+        -1,  1,
+         1, -1,
+         1,  1
     ])
 
     // initialize memory for buffer and populate it. Give
     // open gl hint contents will not change dynamically.
     gl.bufferData( gl.ARRAY_BUFFER, triangles, gl.STATIC_DRAW )
 
-    // create vertex shader
-    let shaderScript = document.getElementById("vertex")
+    return buffer
+}
+
+function makeCopyUniforms() {
+    uRes = gl.getUniformLocation( copyProgram, 'resolution' )
+    gl.uniform2f( uRes, dimensions.width, dimensions.height )
+
+    // get position attribute location in shader
+    copyPosition = gl.getAttribLocation( copyProgram, 'a_pos' )
+    // enable the attribute
+    gl.enableVertexAttribArray( copyPosition )
+    // this will point to the vertices in the last bound array buffer.
+    // In this example, we only use one array buffer, where we're storing 
+    // our vertices. Each vertex will have to floats (one for x, one for y)
+    gl.vertexAttribPointer( copyPosition, 2, gl.FLOAT, false, 0,0 )
+}
+
+function makeSimulationPhase(){
+    makeSimulationShaders()
+    makeSimulationBuffer()
+    makeSimulationUniforms()
+}
+
+function makeSimulationShaders() {
+    let shaderScript = document.getElementById('simulationVertex')
     let shaderSource = shaderScript.text
-    const vertexShader = gl.createShader( gl.VERTEX_SHADER )
-    gl.shaderSource( vertexShader, shaderSource );
+    let vertexShader = gl.createShader( gl.VERTEX_SHADER )
+    gl.shaderSource( vertexShader, shaderSource )
     gl.compileShader( vertexShader )
 
     // create fragment shader
-    shaderScript = document.getElementById("fragment")
+    shaderScript = document.getElementById('simulationFragment')
     shaderSource = shaderScript.text
-    const fragmentShader = gl.createShader( gl.FRAGMENT_SHADER )
-    gl.shaderSource( fragmentShader, shaderSource );
-    gl.compileShader( fragmentShader )
-
-    // create shader program
-    program = gl.createProgram()
-    gl.attachShader( program, vertexShader )
-    gl.attachShader( program, fragmentShader )
-    gl.linkProgram( program )
-    gl.useProgram( program )
-
-    /* ALL ATTRIBUTE/UNIFORM INITIALIZATION MUST COME AFTER 
-    CREATING/LINKING/USING THE SHADER PROGAM */
-
-    // find a pointer to the uniform "time" in our fragment shader
-    uTime = gl.getUniformLocation( program, "time" ) 
-    uRes = gl.getUniformLocation( program, "resolution" )
-    gl.uniform2f( uRes, window.innerWidth, window.innerHeight )
-
-    // get position attribute location in shader
-    const position = gl.getAttribLocation( program, "a_position" )
-    // enable the attribute
-    gl.enableVertexAttribArray( position )
-    // this will point to the vertices in the last bound array buffer.
-    // In this example, we only use one array buffer, where we're storing 
-    // our vertices
-    gl.vertexAttribPointer( position, 2, gl.FLOAT, false, 0, 0)
-
-
-    // ====== tweakpane ======
-
-    const PARAMS = {
-    };
-
-    // TODO: send tweakpane vars to gl buffer
+    const simulationFragmentShader = gl.createShader( gl.FRAGMENT_SHADER )
+    gl.shaderSource( simulationFragmentShader, shaderSource )
+    gl.compileShader( simulationFragmentShader )
+    console.log( gl.getShaderInfoLog(simulationFragmentShader) )
     
-    const pane = new Tweakpane.Pane();
+    // create render program that draws to screen
+    simulationProgram = gl.createProgram()
+    gl.attachShader( simulationProgram, vertexShader )
+    gl.attachShader( simulationProgram, simulationFragmentShader )
 
-    // ====== mouse internaction ======
+    transformFeedback = gl.createTransformFeedback()
+    gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, transformFeedback)
+    gl.transformFeedbackVaryings( simulationProgram, ["o_vpos"], gl.SEPARATE_ATTRIBS )
 
-    const uCursorPos = gl.getUniformLocation(program, "u_cursor_position")
-
-    window.addEventListener("mousemove", e => {
-        cursorPos = getMousePos(e, canvas)
-        gl.uniform2f(uCursorPos, cursorPos.x, cursorPos.y);
-    })
-
-
-    // ====== start render loop ======
-    render();
+    gl.linkProgram( simulationProgram )
+    gl.useProgram(  simulationProgram )
 }
 
-function hex2rgb(hex) {
-    validator = /^#?[0-9A-F]{6}$/i // regex pattern for validating hex colors
+function makeSimulationBuffer() {
+    // create a buffer object to store vertices
+    buffer1 = gl.createBuffer()
+    buffer2 = gl.createBuffer()
 
-    var color = {
-        r: 0.0,
-        g: 0.0,
-        b: 0.0,
-    }
+    // point buffer at graphic context's ARRAY_BUFFER
+    gl.bindBuffer( gl.ARRAY_BUFFER, buffer1 )
+    // we will be constantly updating this buffer data
+    gl.bufferData( 
+        gl.ARRAY_BUFFER, 
+        new Float32Array([0,0,1,0]), 
+        gl.DYNAMIC_COPY 
+    )
 
-    if (validator.test(hex) != true) {
-        console.warn("hex2rgb error: color \"" + hex + "\" failed validation")
-        hex = "#000000"
-        //return color
-    }
+    gl.bindBuffer( gl.ARRAY_BUFFER, buffer2 )
 
-    color.r = Number("0x" + hex.substr(-6, 2))/255
-    color.g = Number("0x" + hex.substr(-4, 2))/255
-    color.b = Number("0x" + hex.substr(-2, 2))/255
-
-    return [color.r, color.g, color.b]
+    gl.bufferData( gl.ARRAY_BUFFER, antCount*16, gl.DYNAMIC_COPY )
 }
 
-function getMousePos(event, canvas) { // normalize cursor coordinates
-    return {
-        x: event.clientX / canvas.width,
-        y: 1-(event.clientY / canvas.height),
-    }
+function makeSimulationUniforms() {
+    uRes = gl.getUniformLocation( simulationProgram, 'resolution' )
+    gl.uniform2f( uRes, gl.drawingBufferWidth, gl.drawingBufferHeight )
+     
+    // get position attribute location in shader
+    simulationPosition = gl.getAttribLocation( simulationProgram, 'a_pos' )
+    // enable the attribute
+    gl.enableVertexAttribArray( simulationPosition )
+
+    gl.vertexAttribPointer( simulationPosition, 4, gl.FLOAT, false, 0,0 )
 }
 
-// keep track of time via incremental frame counter
-let time = 0
+function makeTextures() {
+    textureBack = gl.createTexture()
+    gl.bindTexture( gl.TEXTURE_2D, textureBack )
+    
+    // these two lines are needed for non-power-of-2 textures
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE )
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE )
+    
+    // how to map when texture element is less than one pixel
+    // use gl.NEAREST to avoid linear interpolation
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST )
+    // how to map when texture element is more than one pixel
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    
+    // specify texture format, see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/texImage2D
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, dimensions.width, dimensions.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null )
+
+    textureFront = gl.createTexture()
+    gl.bindTexture( gl.TEXTURE_2D, textureFront )
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE )
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE )
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST )
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST )
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, dimensions.width, dimensions.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null )
+
+    // Create a framebuffer and attach the texture.
+    framebuffer = gl.createFramebuffer()
+}
+
 function render() {
-    // schedules render to be called the next time the video card requests 
-    // a frame of video
     window.requestAnimationFrame( render )
 
-    // update time on CPU and GPU
-    time++
-    gl.uniform1f( uTime, time )
+    gl.useProgram( simulationProgram )
+
+    gl.bindFramebuffer( gl.FRAMEBUFFER, framebuffer )
+
+    // use the framebuffer to write to our textureFront texture
+    gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, textureFront, 0 )
+
+    gl.activeTexture( gl.TEXTURE0 )
+    // read from textureBack in our shaders
+    gl.bindTexture( gl.TEXTURE_2D, textureBack )
+
+    // bind our array buffer of vants
+    gl.bindBuffer( gl.ARRAY_BUFFER, buffer1 )
+    gl.vertexAttribPointer( simulationPosition, 4, gl.FLOAT, false, 0,0 )
+    gl.bindBufferBase( gl.TRANSFORM_FEEDBACK_BUFFER, 0, buffer2 )
     
-    // draw triangles using the array buffer from index 0 to 6 (6 is count)
-    gl.drawArrays( gl.TRIANGLES, 0, 6)
+    gl.beginTransformFeedback( gl.POINTS )  
+    gl.drawArrays( gl.POINTS, 0, antCount )
+    gl.endTransformFeedback()
+
+    /* COPY TEXTURE */
+    gl.framebufferTexture2D( gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, textureBack, 0 )
+
+    gl.activeTexture( gl.TEXTURE0 )
+    gl.bindTexture(   gl.TEXTURE_2D, textureFront )
+    
+    gl.useProgram( copyProgram )
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, quad )
+    gl.vertexAttribPointer( copyPosition, 2, gl.FLOAT, false, 0,0 )
+
+    gl.drawArrays( gl.TRIANGLES, 0, 6 )
+    /* END COPY TEXTURE */
+
+    /* COPY TO SCREEN */
+    // use the default framebuffer object by passing null
+    gl.bindFramebuffer( gl.FRAMEBUFFER, null )
+    gl.viewport( 0,0,gl.drawingBufferWidth, gl.drawingBufferHeight )
+
+    gl.bindTexture( gl.TEXTURE_2D, textureFront )
+
+    // use our drawing (copy) shader
+    gl.useProgram( copyProgram )
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, quad )
+    gl.vertexAttribPointer( copyPosition, 2, gl.FLOAT, false, 0,0 )
+
+    // put simulation on screen
+    gl.drawArrays( gl.TRIANGLES, 0, 6 )
+    
+    /* END COPY TO SCREEN */
+
+    // because we copied texture A over to texture B we don't
+    // need to do a swap  
+    let tmp = buffer1;  buffer1 = buffer2;  buffer2 = tmp;
 }
